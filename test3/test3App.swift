e3d488c29e772b7c05f4078e7f0906ab33fa91a6
adamTab <tabievadam@gmail.com>
2022-11-03 20:48:42 +0100

    Initial Commit

---
+++ test3/test3App.swift
@@ -0,0 +1,17 @@
+//
+//  test3App.swift
+//  test3
+//
+//  Created by Адам Табиев on 03.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct test3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
